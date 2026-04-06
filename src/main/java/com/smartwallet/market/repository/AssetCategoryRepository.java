3f7d2aa3fe1161900825a26fab7b239e82a910b7
Mailonap543 <augustomailon4@gmail.com>
2026-04-06 13:43:43 -0300

    feat: add market module with API v1 - entities, repositories, controller, migrations

--- src/main/java/com/smartwallet/market/repository/AssetCategoryRepository.java
+++ src/main/java/com/smartwallet/market/repository/AssetCategoryRepository.java
@@ -1,8 +1,6 @@
 package com.smartwallet.market.repository;
 
 import com.smartwallet.market.entity.AssetCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +11,6 @@ import java.util.Optional;
 public interface AssetCategoryRepository extends JpaRepository<AssetCategory, Long> {
 
     Optional<AssetCategory> findByCode(String code);
-    
-    Page<AssetCategory> findByCode(String code, Pageable pageable);
     
     List<AssetCategory> findAllByOrderByDisplayOrder();
     
